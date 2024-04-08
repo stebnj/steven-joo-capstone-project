2cffefa5919c283106d49a91de6df0b5d884aa19
steven <steven.joo@live.ca>
2024-04-08 02:43:56 -0700

    add heading

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 import './Header.scss'
 
 export default function Header() {
   return (
-    <Header> 
-      
-    </Header>
+    <header> 
+      <section> 
+        <section>
+          <Link>
+            <h1>NEST</h1>
+          </Link>
+        </section>
+      </section>
+    </header>
   )
 }
