40f48a1022d1cf25b613065fcba6b4c5be916caf
steven <steven.joo@live.ca>
2024-04-11 03:17:53 -0700

    change placeholder

--- client/src/components/ListingsList/ListingsList.js
+++ client/src/components/ListingsList/ListingsList.js
@@ -3,33 +3,50 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 
+
 export default function ListingsList() {
-    const [listings, setListingss] = useState([])
-    
+    const initialSearchTerm = ''
+    const [listings, setListings] = useState([])
+    const [error, setError] = useState('')
     const [isLoading, setIsLoading] = useState(false)
+    const [loading, setLoading] = useState(false);
+    const [searchTerm, setSearchTerm] = useState(initialSearchTerm)
+
+    const handleSearch = async (e) => {
+        e.preventDefault()
+
+        setLoading(true);
+        setError('')
+        setListings([])
+
+        const school = searchTerm.trim().toLowerCase().replace(/\s+/g, '-')
+
+        try {
+            const response = await axios.get(`http://localhost:8080/listings/near-${school}`)
+            setListings(response.data)
+        } catch (err) {
+            console.error('Error fetching listings:', err);
+            setError('Failed to load listings for ' + searchTerm);
+        } finally {
+            setLoading(false);
+            setSearchTerm(initialSearchTerm);
+        }
+    }
 
 
-    useEffect(() => {
-        const getListings = async () => {
-            setIsLoading(true);
-            try{
-                const response = await axios.get(`http://localhost:8080/listings`)
-                setListingss(response.data);
-            } catch(err) {
-                console.error(`Error fetching listings:`, err);
-            } finally {
-                setIsLoading(false)
-            }
-        };
-
-        getListings()
-
-
-    }, [])
 
   return (
     <div>
-        {isLoading && <p>Loading...</p>}
+         <input
+        type="text"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        placeholder="Enter Your University"
+            />
+        <button onClick={handleSearch}>Search</button>
+
+        {loading && <p>Loading...</p>}
+        {error && <p className="error">{error}</p>}
         <ul>
             {listings.map((listing) => (
                 <li key={listing.id}>
