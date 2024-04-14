e9d9bff7a3d2ac37ce58092f605f3e22a5468f90
steven <steven.joo@live.ca>
2024-04-14 10:04:23 -0700

    import login scss to js

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,67 +1,61 @@
-import React, { useState } from 'react'
-import { useRef } from 'react'
-import { useAuth } from '../../contexts/AuthContext'
-import { Link, useNavigate } from 'react-router-dom'
-
+import React, { useState } from "react";
+import { useRef } from "react";
+import { useAuth } from "../../contexts/AuthContext";
+import { Link, useNavigate } from "react-router-dom";
+import "./Login.scss";
 
 export default function Login() {
-    const emailRef = useRef()
-    const passwordRef = useRef()
-    const { login } = useAuth()
-    const [error, setError] = useState('')
-    const [loading, setLoading] = useState(false)
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const { login } = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    async function handleSubmit(e) {
-        e.preventDefault()
+  async function handleSubmit(e) {
+    e.preventDefault();
 
-        try{
-            setError('')
-            setLoading(true)
-            await login(emailRef.current.value, passwordRef.current.value)
-            navigate('/dashboard')
-        } catch (error) {
-            setError('Failed to sign in')
-        }
-       setLoading(false)
+    try {
+      setError("");
+      setLoading(true);
+      await login(emailRef.current.value, passwordRef.current.value);
+      navigate("/dashboard");
+    } catch (error) {
+      setError("Failed to sign in");
     }
-
-    
+    setLoading(false);
+  }
 
   return (
-    <main>
-        <section>
-            <h2>
-                Log In
-            </h2>
-            {error}
-        </section>
-        <section>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>
-                        Email
-                    </label>
-                    <input type="email" ref={emailRef} required />    
-                </div>
-                <div>
-                    <label>
-                        Password
-                    </label>
-                    <input type="password" ref={passwordRef} required/>    
-                </div>
-                <button disabled={loading} type="submit">Log In</button>
-            </form>
-        </section>
-        <section>
-            <p>Don't have an account?<Link to="/signup">Sign up</Link> </p>
-        </section>
-        <section>
-            <Link to="/forgot-password">
-                Forgot Password?
-            </Link>
-        </section>
+    <main className="login">
+      <section className="login__heading-container">
+        <h2 className="login__heading">Log In</h2>
+        {error}
+      </section>
+      <section className="login__actions">
+        <form className="login__form" onSubmit={handleSubmit}>
+          <div className="login__content">
+            <label className="login__label">Email</label>
+            <input className="login__input" type="email" ref={emailRef} required />
+          </div>
+          <div className="login__content">
+            <label className="login__label">Password</label>
+            <input className="login__input"  type="password" ref={passwordRef} required />
+          </div>
+          <button className="login__button"  disabled={loading} type="submit">
+            Log In
+          </button>
+        </form>
+      </section>
+      <section className="login__signup-container">
+        <p className="login__signup">
+          Don't have an account?<Link to="/signup"> Sign up</Link>{" "}
+        </p>
+      </section>
+      <section className="login__forgot-container">
+        <Link to="/forgot-password">Forgot Password?</Link>
+      </section>
     </main>
-  )
+  );
 }
