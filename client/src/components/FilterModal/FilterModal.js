0e3ec1f37d5204b1127fe48a3aaa2e9551182dbd
steven <steven.joo@live.ca>
2024-04-14 09:02:22 -0700

    add styling to modal mobile

--- client/src/components/FilterModal/FilterModal.js
+++ client/src/components/FilterModal/FilterModal.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import xicon from '../../assets/icons/close-24px.svg'
+import './FilterModal.scss'
 
 export default function FilterModal ({ isOpen, onClose, onApplyFilters }) {
     if(!isOpen){
@@ -18,27 +20,38 @@ export default function FilterModal ({ isOpen, onClose, onApplyFilters }) {
     }
 
   return (
-    <div className='modal'>
-        <div className='modal__heading-container'>
-            <h2 className='modal__heading'>
-                Filter Listings
-            </h2>
-        </div>
-        <div className='modal__form-container'>
-            <form className='modal__form' onSubmit={handleApply}>
-                <input className='modal__input' name="minRent" type="number" placeholder='Min Rent' />
-                <input className='modal__input' name="maxRent" type="number" placeholder='Max Rent' />
-                <select className='modal__select' name="bedRange">
-                    <option className='modal__option' value="">Any</option>
-                    <option className='modal__option' value="1 Bed">1 Bed</option>
-                    <option className='modal__option' value="2 Beds">2 Beds</option>
-                    <option className='modal__option' value="3 Beds">3 Beds</option>
-                </select>
-                <button className='modal__button' type="submit">Apply Filters</button>
-                <button className='modal__button' type="button" onClick={onClose}>Close</button>
-            </form>
+    <section className='modal'>
+        <div className='modal__overlay'>
+            <section className='modal__heading-container'>
+                <h2 className='modal__heading'>
+                    Filter Listings
+                </h2>                    
+                <img src={xicon} className='modal__image' onClick={onClose}
+                    />
+            </section>
+            <section className='modal__form-container'>
+                <form className='modal__form' onSubmit={handleApply}>
+                    <div className='modal__actions'>
+                        <div className='modal__input-container'>
+                            <input className='modal__input' name="minRent" type="number" placeholder='Min Rent' />
+                            <input className='modal__input' name="maxRent" type="number" placeholder='Max Rent' />
+                        </div>
+                        <select className='modal__select' name="bedRange">
+                            <option className='modal__option' value="">Any</option>
+                            <option className='modal__option' value="1 Bed">1 Bed</option>
+                            <option className='modal__option' value="2 Beds">2 Beds</option>
+                            <option className='modal__option' value="3 Beds">3 Beds</option>
+                        </select>
+                    </div>
+                    <div className='modal__button-container'>
+                        <button className='modal__button modal__cancel'  type="submit" onClick={onClose}>Cancel</button>
+                        <button className='modal__button' type="submit">Apply Filters</button>
+                    </div>
+                        
+                </form>
+            </section>
         </div>
         
-    </div>
+    </section>
   )
 }
