356c4c4e8f0629295f2243d28ffc9a22caa494a4
steven <steven.joo@live.ca>
2024-04-14 05:49:17 -0700

    add styling to header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import profile from '../../assets/icons/Avatar.png'
+import logo from '../../assets/logo/nest-logo.svg'
 import { Link } from 'react-router-dom'
 
 import './Header.scss'
@@ -10,40 +11,37 @@ export default function Header() {
       <nav className='nav-bar'> 
         <section className='nav-bar__heading-container'>
           <Link to="/" className='nav-bar__link'>
-            <h1 className='nav-bar__heading'>NEST</h1>
+            <img
+              src={logo}
+              className='nav-bar__heading' />
           </Link>
         </section>
         <section className='nav-bar__lists-container'>
-          <ul className='nav-bar__lists'>
-            <Link to="/" className='nav-bar__link'>
-              <li className='nav-bar__item'>
-                Home
-              </li>
-            </Link>
-            <Link className='nav-bar__link'>
-              <li className='nav-bar__item'>
-                Listings
-              </li>
-            </Link>
-          </ul>
-        </section>
-        <section className='nav-bar__right-container'>
           <div className='nav-bar__profile'>
             <Link to="/dashboard">  
               <img
                 className='nav-bar__profile-img'
                 src={profile} />
             </Link>
-          
           </div>
-          <div className='nav-bar__button-container'>
-            <Link to="/signup">
-              <button className='nav-bar__button'>
-                  SIGN UP
-              </button>
-            </Link>  
-          </div>
+          <ul className='nav-bar__lists'>
+            <Link to="/dashboard" className='nav-bar__link'>
+              <li className='nav-bar__item'>
+                 My Listings
+              </li>
+            </Link>
+          </ul>
+          <section className='nav-bar__right-container'>
+            <div className='nav-bar__button-container'>
+              <Link to="/login">
+                <button className='nav-bar__button'>
+                    Log in
+                </button>
+              </Link>  
+            </div>
+          </section>
         </section>
+
       </nav>
     </header>
   )
