4bfdbda615c786d3ff88c32935e8ff910e454748
steven <steven.joo@live.ca>
2024-04-14 08:33:02 -0700

    apply classes to filter modal

--- client/src/components/FilterModal/FilterModal.js
+++ client/src/components/FilterModal/FilterModal.js
@@ -18,24 +18,27 @@ export default function FilterModal ({ isOpen, onClose, onApplyFilters }) {
     }
 
   return (
-    <div>
-        <div>
-            <h2>
+    <div className='modal'>
+        <div className='modal__heading-container'>
+            <h2 className='modal__heading'>
                 Filter Listings
             </h2>
-            <form onSubmit={handleApply}>
-                <input name="minRent" type="number" placeholder='Min Rent' />
-                <input name="maxRent" type="number" placeholder='Max Rent' />
-                <select name="bedRange">
-                    <option value="">Any</option>
-                    <option value="1 Bed">1 Bed</option>
-                    <option value="2 Beds">2 Beds</option>
-                    <option value="3 Beds">3 Beds</option>
+        </div>
+        <div className='modal__form-container'>
+            <form className='modal__form' onSubmit={handleApply}>
+                <input className='modal__input' name="minRent" type="number" placeholder='Min Rent' />
+                <input className='modal__input' name="maxRent" type="number" placeholder='Max Rent' />
+                <select className='modal__select' name="bedRange">
+                    <option className='modal__option' value="">Any</option>
+                    <option className='modal__option' value="1 Bed">1 Bed</option>
+                    <option className='modal__option' value="2 Beds">2 Beds</option>
+                    <option className='modal__option' value="3 Beds">3 Beds</option>
                 </select>
-                <button type="submit">Apply Filters</button>
-                <button type="button" onClick={onClose}>Close</button>
+                <button className='modal__button' type="submit">Apply Filters</button>
+                <button className='modal__button' type="button" onClick={onClose}>Close</button>
             </form>
         </div>
+        
     </div>
   )
 }
