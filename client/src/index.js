f1c752298210dfcfd1568dffa029677d93f77d88
steven <steven.joo@live.ca>
2026-03-15 06:18:05 -0700

    docs

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App asdfasfdafasdfsafsafsdfsasdfa />
+    <App asdfasdfasdfasfasdfasfasdfasfdasdfasdfasfdasfasfdas />
   </React.StrictMode>
 );
 
